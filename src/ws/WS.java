d1326d909c299ac851016b5c947eb8dfc771403f
JAIME-JESUS TEIXIDÓ FERNÁNDEZ <jaimejesusteifer@monlau.es>
2017-10-18 17:31:06 +0200

    Creado Json OBJ, y refactorizado codigo

--- src/ws/WS.java
+++ src/ws/WS.java
@@ -1,51 +1,15 @@
 package ws;
 
-import com.google.gson.Gson;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
-
-
-
 public class WS {
 
     public static void main(String[] args) throws Exception {
-       String JsonURL = "http://localhost/WebService/Consultas.php?format=json&code=1111&action=SelectDatos";
+        String JsonURL = "http://localhost/WebService/query.php?code=1111&action=getall";
         System.out.println("Get Json from: " + JsonURL);
         System.out.println("-----------------------------------------------------------------");
-        String JsonString = callURL(JsonURL);
-        Gson gson = new Gson();
-        ObjetoT json = gson.fromJson(JsonString, ObjetoT.class);  
-        System.out.println(json.getNum1() + "-->" + json.getNum2());
+        Json json = new Json(JsonURL);
+        
+        System.out.println(json.toString());
+        //System.out.println(json.getNum1() + "-->" + json.getNum2());
     }
-    public static String callURL(String myURL) {
-		StringBuilder sb = new StringBuilder();
-		URLConnection urlConn = null;
-		InputStreamReader in = null;
-		try {
-			URL url = new URL(myURL);
-			urlConn = url.openConnection();
-			if (urlConn != null)
-				urlConn.setReadTimeout(60 * 1000);
-			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
-			}
-		in.close();
-		} catch (Exception e) {
-			throw new RuntimeException("Exception while calling URL:"+ myURL, e);
-		} 
- 
-		return sb.toString();
-	}
+    
 }
